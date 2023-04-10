ecb0ed94308a62159b869eab38ab0e7349e79304
Iryna Kutsevol <117863253+irkutsio@users.noreply.github.com>
2023-04-10 22:53:01 +0300

    player-done

--- src/js/02-video.js
+++ src/js/02-video.js
@@ -1,1 +1,14 @@
+import Player from '@vimeo/player';
+import throttle from 'lodash.throttle';
 
+const iframe = document.querySelector('iframe');
+const player = new Player(iframe);
+
+player.on('timeupdate', throttle(onCurrentTime, 1000))
+
+function onCurrentTime (data) { 
+    console.log(data)
+    localStorage.setItem('videoplayer-current-time', data.seconds)
+}
+
+player.setCurrentTime(localStorage.getItem('videoplayer-current-time'));
