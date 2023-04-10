dff96b1a5cae9055e7d1fc4794c779ceba5dd340
Iryna Kutsevol <117863253+irkutsio@users.noreply.github.com>
2023-04-10 11:43:58 +0300

    feedback.done

--- src/js/03-feedback.js
+++ src/js/03-feedback.js
@@ -1,1 +1,34 @@
+import throttle from "lodash.throttle";
 
+const formEl = document.querySelector('.feedback-form');
+const inputEl = formEl.querySelector('input[name="email"]');
+const textareaEl = formEl.querySelector('textarea[name="message"]');
+const USER_DATA = 'feedback-form-state';
+
+formEl.addEventListener('input', throttle(onInput, 500));
+formEl.addEventListener('submit', onSubmit);
+
+function onInput(event) {
+  const userData = {
+    email: inputEl.value,
+    message: textareaEl.value,
+  };
+  localStorage.setItem(USER_DATA, JSON.stringify(userData));
+};
+
+const savedData = JSON.parse(localStorage.getItem(USER_DATA));
+if (savedData) {
+    inputEl.value = savedData.email;
+    textareaEl.value = savedData.message;
+  };
+
+
+function onSubmit(event) {
+  event.preventDefault();
+  event.currentTarget.reset();
+  const savedData = localStorage.getItem(USER_DATA);
+  if(savedData){
+    console.log(JSON.parse(savedData));
+  };
+  localStorage.removeItem(USER_DATA);
+};
